c0c42ba4378fb4046675808de403a718d83f5651
Katada10 <katadafreije@gmail.com>
2020-10-05 08:46:58 -0400

    Added correct files

--- Part 2/Size.java
+++ Part 2/Size.java
@@ -1,35 +1,20 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Size{
-	public static int numberOfElements;
-
-	public static void setNumberOfElements(){
-		numberOfElements = 1000000;
-	}
-
-	public static int getNumberOfElements(){
-		Size.setNumberOfElements();
-		return numberOfElements;
-	}
-
-	public static char getRandomChar(){
-		Random random = new Random();
-		char letter = (char)(random.nextInt(26)+'a');
-		return letter;
-	}
+	public static int numberOfElements = 900000;
+	public static String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 	public static double testArrayTime(){
 		ArrayList<Character> testArray = new ArrayList<Character>();
 		long start = System.currentTimeMillis();
-		for(int i = 0; i<Size.getNumberOfElements(); i++){
-			char letter = Size.getRandomChar();
-			testArray.add(letter);}
+		for(int i = 0; i<numberOfElements; i++){
+			char c = (char) alphabet.charAt(i%26);
+			testArray.add(c);}
 		long stop = System.currentTimeMillis();
 		double timeElapsed = ((double) stop - start)/1000;
 		return timeElapsed;}
 	
 	public static void main(String[] args){
-		System.out.println("It took "+testArrayTime()+" seconds to populate array");
+		System.out.println(testArrayTime());
 	}
 }
